7dbdc522e55243cbeca81369f0910957465bd365
Pavel Afanasiev <afanasyevpv9@gmail.com>
2022-09-05 16:53:18 +0300

    Применяем GCD

---
+++ AppWithQueues/SecondViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.swift
+//  AppWithQueues
+//
+//  Created by Павел Афанасьев on 05.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        
+        imageURL = URL(string: "https://www.wallpapers13.com/wp-content/uploads/2016/01/Sunset-sea-shore-sea-waves-rocky-mountains-red-sky-dark-cloud-Beautiful-HD-Wallpaper-840x525.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
+    }
+}
